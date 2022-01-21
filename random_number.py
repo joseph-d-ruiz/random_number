247d2a5e95778d618c1012d021b0f0027411ae4f
Joseph D Ruiz <joseph.ruiz@me.com>
2022-01-21 11:51:27 -0700

    Refactoring away from recursive call
    Calos helped me with finding a way to loop the game without using up
    too much resouces

--- random_number.py
+++ random_number.py
@@ -3,27 +3,26 @@ import random
 #player setup
 player = str(input("who are you?")).title()
 
-
-def main():
-
 #how the number is picked
-    def guess(x):
-        random_number = random.randint(1, x)
-        guess = 0
-        while guess != random_number:
-            guess = int(input(f"Please guess a number between 1 and {x}: "))
-            if guess < random_number:
-                print("Guess again to low ")
-            elif guess > random_number:
-                print("Guess again too high ")
-        print("Super cool " + (player) + f", you have guess the number {random_number} correctly ")
+
+
+def play_guessing_game(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Please guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Guess again to low ")
+        elif guess > random_number:
+            print("Guess again too high ")
+    print("Super cool " + (player) + f", you have guess the number {random_number} correctly ")
         
-    guess(100)
-    repeat = input("Would like to play again? Yes or No: ").lower()
-    if repeat == 'yes':
-        main()
-    else:
-        print("See ya")
-        exit()
+def main():
+    while True:
+        play_guessing_game(100)
+        repeat = input("Would like to play again? Yes or No: ").lower()
+        if repeat != 'yes':
+            print("See ya")
+            exit()
 
 main()
